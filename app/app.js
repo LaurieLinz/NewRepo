ced0e1f69ecc9b7103cfe7931b3005a5e72084b6
Laurie Linz <lvlinz@comcast.net>
2016-09-21 21:25:55 -0600

    Added todo app

--- app/app.js
+++ app/app.js
@@ -6,30 +6,30 @@ import $ from 'jquery';
 window.$ = window.jQuery = $;
 require('bootstrap');
 
-// import our styles
-import './stylesheets/base.scss';
+// // import our styles
+// import './stylesheets/base.scss';
 import _ from 'underscore';
+import todoController from './pages/todo';
+
 
 // on document load
 $(function(){
 
   console.log('%c App Started', 'color:green');
 
-  // add some html content
-  $('#root').html('<button class="btn btn-default" type="submit">Button</button>');
-
-  // use a template
+  // set default template settings
   _.templateSettings = {
     evaluate:    /{{([\s\S]+?)}}/g,
     interpolate: /{{-([\s\S]+?)}}/g,
     escape:      /{{=([\s\S]+?)}}/g
   };
 
-  var template = '<button class="btn btn-default" type="submit">{{= name}}</button>';
-  var compiledTemplate = _.template(template);
-  $('#root').html(compiledTemplate({name: '<fruit>'}));
+  // Kick off the app!
+  // which page are we on??
+  if (window.location.pathname === '/pages/todo.html') {
+    todoController.init();
+  }
+  
 
-  // optionally animate that content
-  $('#root button').animate({fontSize: 50}, 1000);
 
 });
---
+++ app/pages/todo.js
@@ -0,0 +1,94 @@
+
+var $ = window.$;
+var Handlebars = window.Handlebars;
+import lscache from 'lscache';
+
+
+var database = [];
+var model = {
+  init: function(){
+    var savedData = lscache.get('todos');
+    if (savedData) {
+      database = savedData;
+    } else {
+      database = [];
+    }  
+  },
+  save: function(){
+    var dataToSave = JSON.stringify(database);
+    lscache.set('todos', dataToSave);
+  },
+  get: function(){
+    return database;
+  }
+};
+
+var view = $('script[type="text/x-template"]').html();
+
+var controller = {
+  init: function () {
+    model.init();
+    //cache some selectors
+    controller.addButton = $('.btn-add');
+    // start everything up
+    controller.compiledTemplate = Handlebars.compile(view);
+    controller.renderTemplates();
+  },
+  render: function (compiledTodos) {
+    //do all the visual stuff
+    controller.destroyEventHandlers();
+    $('.todo-list').html(compiledTodos.join('')); 
+    controller.createEventHandlers();
+  },
+  renderTemplates: function(){
+    var compiledTodos = [];
+    model.get().forEach(function(item, index) {
+      item.id = index + 1;
+      var renderedTodo = controller.compiledTemplate(item);
+      compiledTodos.push(renderedTodo);
+    });
+    controller.render(compiledTodos);
+    model.save();
+  },
+  destroyEventHandlers: function(){
+    controller.addButton.off();
+    $('input[type="checkbox"]').off();
+    $('.close').off();
+  },
+  createEventHandlers: function() {
+    controller.addButton.on('click', controller.addTodoHandler);
+    $('input[type="checkbox"]').on('change', controller.checkedHandler);
+    $('.close').on('click', controller.removeHandler);
+  },
+  removeHandler: function(event){
+    //which one was clicked??
+    var index = $(event.currentTarget).parent().parent().index();
+    //update the database
+    model.get().splice(index, 1);
+    // update the view
+    controller.renderTemplates();
+  },
+  checkedHandler: function (event){
+    // which checkbox?
+    var index = $(event.currentTarget).parent().parent().index();
+    // update the database
+    model.get()[index].completed = !model.get()[index].completed;
+    // view updates automatically yay HTML!
+    model.save();
+  },
+  addTodoHandler: function() {
+    var newTitle = $('.add-input').val();
+    if (newTitle === '') return;
+    model.get().push({
+      title: newTitle,
+      complete: false
+    });
+    $('.add-input').val('');
+    controller.renderTemplates();
+  }
+};
+
+module.exports = controller;
+
+
+
