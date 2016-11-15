e6271e3366e59a84cb9c220313378237ab9e21d5
Laurie Linz <lvlinz@comcast.net>
2016-11-14 21:15:12 -0700

    Sketch working and added tools

--- app/pages/sketch.js
+++ app/pages/sketch.js
@@ -7,6 +7,9 @@ var app = {
 
   render: function(){
     $('#simple_sketch').sketch();
+    $.each(['#f00', '#ff0', '#0f0', '#ff8c1f', '#00f', '#7E20B0', '#000', '#fff'], function() {
+      $('#colors-demo').append('<a href="#simple_sketch" data-color="' + this + '" style="width: 25px; background: ' + this + ';"></a> ');
+    });
   } 
 };
 
